1193a268e42b881c2da2f9a6dbbff910a2423e89
unknown <vindhyajee07@gmail.com>
2019-10-18 13:23:34 +0530

    first commit

--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -3,12 +3,13 @@ package com.javainuse.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.javainuse.model.Employee;
 
 @RestController
 public class TestController {
-
+	private  final Logger logger = LoggerFactory.getLogger(TestController.class);
 	@RequestMapping(value = "/employee", method = RequestMethod.GET)
 	public Employee firstPage() {
 
@@ -17,7 +18,7 @@ public class TestController {
 		emp.setDesignation("manager");
 		emp.setEmpId("1");
 		emp.setSalary(3000);
-
+		logger.info("--Application Started--");
 		return emp;
 	}
 
